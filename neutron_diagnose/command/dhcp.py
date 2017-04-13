223d0d5edaf77a8264ce06d68adaa1b329bb9f1b
shihz <shihz@fiberhome.com>
2017-04-13 14:55:43 +0800

    Add a example in dhcp.py
    Change-Id: I035e2e8da493c30ab1f507101ee75dce20b67d8c

--- neutron_diagnose/command/dhcp.py
+++ neutron_diagnose/command/dhcp.py
@@ -12,6 +12,7 @@
 #
 
 from neutron_diagnose.command import commandmanager
+from neutron_diagnose.common import exceptions
 from neutron_diagnose.common import utils
 from neutron_diagnose.i18n import _
 
@@ -43,10 +44,42 @@ class CheckVmIp(commandmanager.ShowOne):
         ssh_client = self.app.client_manager.ssh
 
         server = compute_client.servers.get(parsed_args.instance_id)
-        network = network_client.find_network()
+        project_id = server.tenant_id
+        host = server._info.get('OS-EXT-SRV-ATTR:host')
+        if not host:
+            raise exceptions.DiagnoseException(
+                'the instance: %s has no host.', server.id)
+        network_name = None
+        port_mac = None
+        port_id = None
+        for network, details in server._info.get('addresses', dict()).items():
+            for ip_infs in details:
+                if ip_infs.get('addr') == parsed_args.fixed_ip:
+                    network_name = network_name
+                    port_mac = ip_infs.get('OS-EXT-IPS-MAC:mac_addr')
+        port_filters = {'device_owner': 'compute:nova',
+                        'device_id': server.id,
+                        'mac_address': port_mac,
+                        'project_id': project_id}
+        ports = network_client.ports(**port_filters)
+        port_len = 0
+        for port in ports:
+            port_len += 1
+            fixed_ips = port.fixed_ips
+            for ip_info in fixed_ips:
+                if ip_info['ip_address'] == parsed_args.fixed_ip:
+                    port_id = port.id
+                    break
+
+        if port_len > 1:
+            raise exceptions.DiagnoseException(
+                'The tenant: %s has more one instances use same IP and MAC,'
+                'it does not support in current version.' % project_id)
+        if not port_id:
+            raise exceptions.DiagnoseException(
+                "Can't retrieve related Neutron port id.")
         result = {
-            'vif': 'OK',
-            'ovs flow': 'OK',
-            'iptables rule': 'OK'
+            'host': host,
+            'port id': port_id,
         }
         return commandmanager.set_result(result)
